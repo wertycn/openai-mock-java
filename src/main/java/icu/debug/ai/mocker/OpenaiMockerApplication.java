045b5c96fea9de148054d9d12c07f75ac268c5d0
debug.icu <hajnxg@126.com>
2024-03-18 23:07:06 +0800

    feat: project init

---
+++ src/main/java/icu/debug/ai/mocker/OpenaiMockerApplication.java
@@ -0,0 +1,13 @@
+package icu.debug.ai.mocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenaiMockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenaiMockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/icu/debug/ai/mocker/OpenaiMockerApplicationTests.java
@@ -0,0 +1,13 @@
+package icu.debug.ai.mocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenaiMockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
